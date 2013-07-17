581c9906d861a9cb5b391e63d36044b024d1b66b
Adrian Chadd <adrian@freebsd.org>
2013-07-16 18:32:10 -0700

    Flesh out the rest of the initial lifecycle for the client-side.

--- src/clt/clt.c
+++ src/clt/clt.c
@@ -19,13 +19,18 @@
 struct clt_app;
 struct conn;
 
+#define	NUM_CLIENTS_PER_THREAD		128
+
 typedef enum {
 	CONN_STATE_NONE,
 	CONN_STATE_CONNECTING,
 	CONN_STATE_RUNNING,
 	CONN_STATE_ERROR,
-	CONN_STATE_CLOSING
+	CONN_STATE_CLOSING,
+	CONN_STATE_FREEING
 } conn_state_t;
+
+typedef void conn_owner_update_cb(struct conn *c, void *arg, conn_state_t newstate);
 
 struct conn {
 	int fd;
@@ -43,34 +48,18 @@ struct conn {
 		char *buf;
 		int size;
 	} w;
+	struct {
+		conn_owner_update_cb *cb;
+		void *cbdata;
+	} cb;
 };
 
 struct clt_app {
 	pthread_t thr_id;
 	struct fde_head *h;
+	int num_clients;
 	TAILQ_HEAD(, conn) conn_list;
 };
-
-#if 0
-static void
-client_read_cb(int fd, struct fde_comm *fc, void *arg, fde_comm_cb_status s,
-    int retval)
-{
-	struct conn *c = arg;
-
-	fprintf(stderr, "%s: FD %d: %p: s=%d, ret=%d\n",
-	    __func__,
-	    fd,
-	    c,
-	    s,
-	    retval);
-
-	/*
-	 * start the close process
-	 */
-	comm_close(fc);
-}
-#endif
 
 static void
 conn_ev_cleanup_cb(int fd, struct fde *f, void *arg, fde_cb_status status)
@@ -84,12 +73,46 @@ conn_ev_cleanup_cb(int fd, struct fde *f, void *arg, fde_cb_status status)
 	}
 
 	fde_delete(c->parent->h, c->ev_cleanup);
+
+	/* Notify owner that I'm about to be freed */
+	c->cb.cb(c, c->cb.cbdata, CONN_STATE_FREEING);
+
 	if (c->r.buf)
 		free(c->r.buf);
 	if (c->w.buf)
 		free(c->w.buf);
 	TAILQ_REMOVE(&c->parent->conn_list, c, node);
 	free(c);
+}
+
+/*
+ * Initiate shutdown of a given conn.
+ *
+ * This should be called from the owner, as there's no notification
+ * path for this.
+ */
+static void
+conn_close(struct conn *c)
+{
+
+	if (c->state == CONN_STATE_CLOSING)
+		return;
+
+	fprintf(stderr, "%s: %p: called\n", __func__, c);
+
+	c->state = CONN_STATE_CLOSING;
+
+	/* Call comm_close(); when IO completes we'll get notified */
+	/*
+	 * The alternative is to track when we're writing and if we
+	 * are, just wait until we're done
+	 */
+	comm_close(c->comm);
+	c->comm = NULL;
+
+	/*
+	 * The rest of close will occur once the close handler is called.
+	 */
 }
 
 static void
@@ -112,7 +135,7 @@ conn_write_cb(int fd, struct fde_comm *fc, void *arg,
 	/* Any error? Transition; notify upper layer */
 	if (status != FDE_COMM_CB_COMPLETED) {
 		c->state = CONN_STATE_ERROR;
-		/* XXX notify */
+		c->cb.cb(c, c->cb.cbdata, CONN_STATE_ERROR);
 		return;
 	}
 
@@ -127,7 +150,7 @@ conn_write_cb(int fd, struct fde_comm *fc, void *arg,
 		    c->w.size,
 		    nwritten);
 		c->state = CONN_STATE_ERROR;
-		/* XXX notify */
+		c->cb.cb(c, c->cb.cbdata, CONN_STATE_ERROR);
 		return;
 	}
 
@@ -135,6 +158,24 @@ conn_write_cb(int fd, struct fde_comm *fc, void *arg,
 	 * Write some more data
 	 */
 	comm_write(c->comm, c->w.buf, c->w.size, conn_write_cb, c);
+}
+
+/*
+ * Close callback - the close has finished; schedule the
+ * rest of object tidyup.
+ */
+static void
+conn_close_cb(int fd, struct fde_comm *fc, void *arg)
+{
+	struct conn *c = arg;
+
+#if 0
+	fprintf(stderr, "%s: %p: called, scheduling cleanup\n", __func__, c);
+#endif
+
+	/* Schedule the tidyup event */
+	/* XXX should track this to only happen once */
+	fde_add(c->parent->h, c->ev_cleanup);
 }
 
 static void
@@ -149,18 +190,25 @@ conn_connect_cb(int fd, struct fde_comm *fc, void *arg,
 	/* Error? Notify the upper layer; finish */
 	if (status != FDE_COMM_CB_COMPLETED) {
 		c->state = CONN_STATE_ERROR;
-		/* XXX notify */
+		c->cb.cb(c, c->cb.cbdata, CONN_STATE_ERROR);
 		return;
 	}
 
 	/* Success? Start writing data */
 	c->state = CONN_STATE_RUNNING;
 
+	/* Notify owner; they may decide to change state */
+	c->cb.cb(c, c->cb.cbdata, CONN_STATE_RUNNING);
+
+	/* If the caller changed state, don't do a write */
+	if (c->state != CONN_STATE_RUNNING)
+		return;
+
 	comm_write(c->comm, c->w.buf, c->w.size, conn_write_cb, c);
 }
 
 struct conn *
-conn_new(struct clt_app *r)
+conn_new(struct clt_app *r, conn_owner_update_cb *cb, void *cbdata)
 {
 	struct conn *c;
 	int i;
@@ -204,30 +252,63 @@ conn_new(struct clt_app *r)
 		return (NULL);
 	}
 	c->parent = r;
-	c->comm = comm_create(c->fd, r->h, NULL, NULL);
+	c->comm = comm_create(c->fd, r->h, conn_close_cb, c);
 	c->ev_cleanup = fde_create(r->h, -1, FDE_T_CALLBACK,
 	    conn_ev_cleanup_cb, c);
 	comm_set_nonblocking(c->comm, 1);
+
+	c->cb.cb = cb;
+	c->cb.cbdata = cbdata;
+
 	TAILQ_INSERT_TAIL(&r->conn_list, c, node);
 
 	return (c);
 }
 
-void *
-thrclt_new(void *arg)
+/*
+ * Owner related routines
+ */
+
+static void
+thrclt_conn_update_cb(struct conn *c, void *arg, conn_state_t newstate)
 {
 	struct clt_app *r = arg;
-	struct timespec tv;
+
+#if 0
+	fprintf(stderr, "%s: %p: called; state=%d\n", __func__, r, newstate);
+#endif
+
+	/* If we hit an error, we should schedule a close of this object */
+	if (newstate == CONN_STATE_ERROR) {
+		conn_close(c);
+		return;
+	}
+
+	/*
+	 * Freeing? Use this to track the number of open connectons.
+	 */
+	if (newstate == CONN_STATE_FREEING) {
+		if (r->num_clients == 0) {
+			fprintf(stderr, "%s: %p: num_clients=0 ? \n",
+			    __func__, r);
+		} else {
+			r->num_clients--;
+		}
+		fprintf(stderr, "%s: %p: client freed\n", __func__, r);
+	}
+}
+
+int
+thrclt_open_new_conn(struct clt_app *r)
+{
 	struct conn *c;
 	struct sockaddr_in sin;
 	socklen_t slen;
 
-	fprintf(stderr, "%s: %p: created\n", __func__, r);
-
 	/*
 	 * For now, let's create one client object and kick-start it.
 	 */
-	c = conn_new(r);
+	c = conn_new(r, thrclt_conn_update_cb, r);
 
 	/* Start connecting */
 	c->state = CONN_STATE_CONNECTING;
@@ -239,7 +320,25 @@ thrclt_new(void *arg)
 	(void) comm_connect(c->comm, (struct sockaddr *) &sin, slen,
 	    conn_connect_cb, c);
 
+	r->num_clients++;
 
+	return (0);
+}
+
+void *
+thrclt_new(void *arg)
+{
+	struct clt_app *r = arg;
+	struct timespec tv;
+	struct conn *c;
+
+	fprintf(stderr, "%s: %p: created\n", __func__, r);
+
+	/* open up NUM_CLIENTS_PER_THREAD conncetions, stop if we fail */
+	while (r->num_clients < NUM_CLIENTS_PER_THREAD) {
+		if (thrclt_open_new_conn(r) < 0)
+			break;
+	}
 
 	/* Loop around, listening for events; farm them off as required */
 	while (1) {
